b0533e949dba4927310c293580abafa8964c0f70
buiphuoctai <buiphuoctai1405@gmail.com>
2025-03-07 00:01:30 +0700

    create header, footer and sign in, sign up modals

---
+++ nhatthanhland/components/modals/SignInModal.tsx
@@ -0,0 +1,123 @@
+"use client";
+import Image from "next/image";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+
+export default function SignInModal({
+  open,
+  setOpen,
+  onSwitchToSignUp,
+}: {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  onSwitchToSignUp: () => void;
+}) {
+  const handleSwitchToSignUp = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(false); // Close sign in modal
+    onSwitchToSignUp(); // Open sign up modal
+  };
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+          >
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <Image
+                  width={21}
+                  height={21}
+                  alt="Your Company"
+                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                  className="mx-auto h-10 w-auto"
+                />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                  Join or Sign in
+                </h2>
+              </div>
+
+              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form action="#" method="POST" className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm/6 font-medium text-gray-900"
+                    >
+                      Phone number
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="phonenumber"
+                        name="phonenumber"
+                        type="tel"
+                        required
+                        autoComplete="number"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm/6 font-medium text-gray-900"
+                      >
+                        Password
+                      </label>
+                      {/* <div className="text-sm">
+                        <a
+                          href="#"
+                          className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        >
+                          Forgot password?
+                        </a>
+                      </div> */}
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        autoComplete="current-password"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                  Not a member?{" "}
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    onClick={handleSwitchToSignUp}
+                  >
+                    Sign up now
+                  </a>
+                </p>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
